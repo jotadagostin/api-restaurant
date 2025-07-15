95f2a4a92b60a81193af2b780a18a13ee55feb2d
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-07-15 11:03:36 +0200

    setting the validation

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,10 +1,24 @@
 import { NextFunction, Request, Response } from "express";
-import { AppError } from "@/utils/AppError";
-
+import { z } from "zod";
 class ProductsController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
       return response.json({ message: "Ok" });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string({ required_error: "name is required!" }).trim().min(6),
+        price: z.number().gt(0, { message: "value must be greater than 0" }),
+      });
+
+      const { name, price } = bodySchema.parse(request.body);
+
+      return response.status(201).json({ name, price });
     } catch (error) {
       next(error);
     }
